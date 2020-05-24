43aae956c6db6470fd881609463eeb3475724004
farhanoktvn <farhanoktvn@gmail.com>
2020-05-24 20:02:11 +0700

    fix database

--- shareforcare/settings.py
+++ shareforcare/settings.py
@@ -133,9 +133,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = '/login/'
 
 # Heroku settings
-import dj_database_url
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+import django_heroku
+django_heroku.settings(locals())
 
 # LocationField settings
 LOCATION_FIELD = {
